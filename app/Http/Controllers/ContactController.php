14926ed2fcf2a51c6c11302800b0c835157df399
ashik607 <ashik.200607@s.pust.ac.bd>
2024-10-25 22:43:57 +0600

    add Controllers/ContactController.php

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'phone' => 'required|string|max:15',
+            'message' => 'required|string',
+        ]);
+
+        // Create a new contact entry
+        Contact::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'message' => $request->input('message'),
+        ]);
+        Contact::create($request->only(['name', 'email', 'phone', 'message']));
+
+
+        // Redirect or return a response
+        return redirect()->back()->with('success', 'Your message has been sent!');
+    }
+}
+
